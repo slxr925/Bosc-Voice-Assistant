8e5fdcc46bd44e50f7295aaebd2802981c904af0
xuran <slxr925@gmail.com>
2020-08-27 13:46:31 +0800

    第一次提交 包含springboot自带文件

---
+++ src/test/java/com/bosc/voiceassistant/aba/AbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosc.voiceassistant.aba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
