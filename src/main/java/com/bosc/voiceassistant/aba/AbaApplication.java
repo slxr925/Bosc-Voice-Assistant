baac01b136cef822e050160b69dec26cc3f83f4a
xuran <slxr925@gmail.com>
2020-09-01 10:00:57 +0800

    增加远程mysql连接

--- src/main/java/com/bosc/voiceassistant/aba/AbaApplication.java
+++ src/main/java/com/bosc/voiceassistant/aba/AbaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AbaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(AbaApplication.class, args);
     }
 
