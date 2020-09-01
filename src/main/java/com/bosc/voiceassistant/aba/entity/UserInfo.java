d564d205d718e95116d85d45a1bcd824cf5f817a
xuran <slxr925@gmail.com>
2020-09-01 19:33:57 +0800

    mysql配置提交

---
+++ src/main/java/com/bosc/voiceassistant/aba/entity/UserInfo.java
@@ -0,0 +1,22 @@
+package com.bosc.voiceassistant.aba.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+
+/**
+ * @author Xu Ran
+ * @since 2020/9/1 下午6:26
+ */
+//@Data
+//@Entity(name = "User")
+public class UserInfo {
+//    @Id
+//    @GeneratedValue
+//    private int userId;
+//
+//    @Column
+}
---
+++ src/main/java/com/bosc/voiceassistant/aba/service/UserService.java
@@ -0,0 +1,8 @@
+package com.bosc.voiceassistant.aba.service;
+
+/**
+ * @author Xu Ran
+ * @since 2020/9/1 下午7:31
+ */
+public interface UserService {
+}
