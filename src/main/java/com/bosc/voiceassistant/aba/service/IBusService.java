4aa92484958efe5865ec55c9a5f69a4b0ffb2630
xuran <slxr925@gmail.com>
2020-09-13 11:12:45 +0800

    add implement/interface files

---
+++ src/main/java/com/bosc/voiceassistant/aba/service/IBusService.java
@@ -0,0 +1,8 @@
+package com.bosc.voiceassistant.aba.service;
+
+/**
+ * @author Xu Ran
+ * @since 2020/9/13 11:11 上午
+ */
+public interface IBusService {
+}
---
+++ src/main/java/com/bosc/voiceassistant/aba/service/IRepairService.java
@@ -0,0 +1,8 @@
+package com.bosc.voiceassistant.aba.service;
+
+/**
+ * @author Xu Ran
+ * @since 2020/9/13 11:12 上午
+ */
+public interface IRepairService {
+}
