f575669384c5fa9b48e7132d5a31dec15d179723
xuran <slxr925@gmail.com>
2020-09-15 11:20:21 +0800

    add dbconnectioncofig

---
+++ src/main/java/com/bosc/voiceassistant/aba/config/DBHelperConfig.java
@@ -0,0 +1,12 @@
+package com.bosc.voiceassistant.aba.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Xu Ran
+ * @since 2020/9/15 11:19 上午
+ */
+@Configuration
+public class DBHelperConfig {
+
+}
